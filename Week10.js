7ac982cd0d1fd1a9c9599958ca95256cd53be26c
Christina Lujan <christina.lujan89@gmail.com>
2023-04-08 17:41:07 -0700

    final push

--- Week10.js
+++ Week10.js
@@ -5,13 +5,11 @@ document.getElementById('add').addEventListener('click', () => {
     let table = document.getElementById('list');
     let row = table.insertRow(1);
     row.setAttribute('id', `item-${id}`);
-    row.insertCell(0).innerHTML = document.getElementById('new-task').value;
-    row.insertCell(1).innerHTML = `${createdDate.getFullYear()}-${createdDate.getMonth() + 1}-${createdDate.getDate()}`;
-    row.insertCell(2).innerHTML = document.getElementById('new-start-date').value;
-    row.insertCell(3).innerHTML = document.getElementById('new-end-date').value;
-    let actions = row.insertCell(4);
+    row.insertCell(0).innerHTML = document.getElementById('new-state').value;
+    row.insertCell(1).innerHTML = document.getElementById('new-date').value;
+    let actions = row.insertCell(2);
     actions.appendChild(createDeleteButton(id++));
-    document.getElementById('new-task').value = '';
+    document.getElementById('new-state').value = '';
 });
 
 function createDeleteButton(id) {
